a78f07dd73c97ff89b09884e2b379d832cc76412
roderickzapata <roderickzapata@gmail.com>
2024-03-21 15:45:15 -0600

    funcional sin base de datos

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './**/*.html',
+  ],
+
+  theme: {
+    extend: {
+      // colores personalizado
+      colors: {
+        terracotta: '#fe5300',
+        navy: '#003366', // un ejemplo de azul marino
+      },
+    },
+  },
+}
